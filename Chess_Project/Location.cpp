651875c7ec674e971157c37fd18c122b7f98f2e3
Yuval Dahan <yuvaldahn@gmail.com>
2018-12-05 18:42:31 +0200

    Created all Location Class

--- Chess_Project/Location.cpp
+++ Chess_Project/Location.cpp
@@ -1,9 +1,18 @@
 #include "Location.h"
 
-Location::Location()
+Location::Location(char col, char row)
 {
+	if(col >= 'a' && col <= 'z' && row >= '0' && row <= '9')
+	{
+		this->_col = col - 'a';
+		this->_row = row - '0';
+	}
+	else
+	{
+		throw ILLEGAL_INDEX;
+	}
 }
 
-Location::~Location()
-{
-}
+Location::Location(string loc): Location(loc[0], loc[1]) {}
+
+Location::~Location() {}
--- Chess_Project/Location.h
+++ Chess_Project/Location.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <iostream>
+#include "Piece.h"
+
+using std::string;
+
 class Location
 {
+private:
+	int _col;
+	int _row;
 public:
-	Location();
+	Location(char col, char row);
+	Location(string loc);
 	~Location();
 };
-
