3b65cf5f5849fdacc0886b34a75fb427c2fbc7bd
Harel Katz <34490550+HarelKatz@users.noreply.github.com>
2018-12-05 19:54:46 +0200

    added board and did the ctor of board and working on move function

---
+++ Chess_Project/Board.cpp
@@ -0,0 +1,67 @@
+/*
+Authors:	Harel Katz,			Yuval Dahan
+Emails:		harelka2@gmail.com, yuvaldahn@gmail.com
+*/
+#include "Board.h"
+
+Board::Board(string init_board)
+{
+	int pos = 0;
+	for (int i = 0; i < 8; ++i, ++pos)
+	{
+		for (int j = 0; j < 8; ++j, ++pos)
+		{
+			this->_board_arr[i][j] = get_piece(init_board[pos]);
+		}
+	}
+	this->_turn = (init_board[pos]) ? BLACK : WHITE;
+}
+
+void Board::move(Location src, Location dst)
+{
+	Piece* piece = _board_arr[src.get_row()][src.get_col()];
+	if (piece == nullptr)
+	{
+		throw NO_PLAYER_TOOL_IN_SRC;
+	}
+	piece->is_valid_move(src, dst);
+	//the movment delet src,
+}
+
+bool Board::is_check(bool _side)
+{
+	return false;
+}
+
+Piece * Board::get_piece(char p)
+{
+	Side side = WHITE;
+	if (p > 'Z')
+	{
+		side = BLACK;
+		p = p - 'a' + 'A';
+	}
+	switch (p)
+	{
+	case 'K':
+		return King(side);
+		break;
+	case 'Q':
+		return Queen(side);
+		break;
+	case 'R':
+		return Rook(side);
+		break;
+	case 'N':
+		return Knight(side);
+		break;
+	case 'B':
+		return Bishop(side);
+		break;
+	case 'P':
+		return Pawn(side);
+		break;
+	case '#':
+		return nullptr;
+	}
+}
---
+++ Chess_Project/Board.h
@@ -0,0 +1,49 @@
+/*
+Authors:	Harel Katz,			Yuval Dahan
+Emails:		harelka2@gmail.com, yuvaldahn@gmail.com  
+*/
+#pragma once
+#include <iostream>
+#include <string>
+#include "Piece.h"
+#include "Location.h"
+#include "King.h"
+#include "Queen.h"
+#include "Rook.h"
+#include "Knight.h"
+#include "Bishop.h"
+#include "Pawn.h"
+
+using std::string;
+
+enum MyEnum
+{
+	GOOD_MOV,
+	CHESS_MOVE,
+	NO_PLAYER_TOOL_IN_SRC,
+	PLAYER_TOOL_IN_TRGT,
+	SELF_CHESS_MOVE,
+	ILLEGAL_INDEX,
+	ILLEGAL_TOOL_MOVE,
+	SRC_AND_TRGT_SAME,
+	MATE_MOVE
+};
+
+class Board
+{
+
+protected:
+
+	Piece* _board_arr[8][8];
+	Side _turn;
+
+public:
+
+	Board(string );
+	void move(Location src, Location dst);
+	bool is_check(bool _side);
+	Piece* get_piece(char p);
+	Piece* operator[](string loc);
+	Piece* operator[](Location loc);
+
+};
