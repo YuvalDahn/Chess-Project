970aca9413e9b98275e3f2863f4edb9c5864ead0
Yuval Dahan <yuvaldahn@gmail.com>
2018-12-07 18:29:36 +0200

    added a Piece C'tor that recives a Side, fixed the static move functions of Piece

--- Chess_Project/Piece.cpp
+++ Chess_Project/Piece.cpp
@@ -1,5 +1,10 @@
 #include "Piece.h"
 
+Piece::Piece(Side color)
+{
+	this->_color = color;
+}
+
 Piece::~Piece() {}
 
 Side Piece::get_color()
@@ -7,13 +12,45 @@ Side Piece::get_color()
 	return this->_color;
 }
 
-bool Piece::moveStraight(Location src, Location dst)
+bool Piece::moveStraight(Location src, Location dst, Piece* board_arr[][8])
 {
-	return src.get_col() == dst.get_col() || src.get_row() == dst.get_row();
+	if(src.get_col() == dst.get_col() || src.get_row() == dst.get_row())	//check if src and dst are in straight line
+	{
+		for(size_t i = src.get_row(); i < dst.get_row(); i++)				//check if there's something in the way
+		{
+			for(size_t j = src.get_col(); j < dst.get_col(); j++)
+			{
+				if(board_arr[i][j] != nullptr)								//if there is something on the way, it is not a valid move
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	return false;
 }
 
-bool Piece::moveDiagonal(Location src, Location dst)
+bool Piece::moveDiagonal(Location src, Location dst, Piece* board_arr[][8])
 {
-	return src.get_col() - dst.get_col() == src.get_row() - dst.get_row() ||
-		-1 * (src.get_col() - dst.get_col()) == src.get_row() - dst.get_row();
+	int verctical_diff = dst.get_row() - src.get_row();					//row desired move
+	int horizontal_diff = dst.get_col() - src.get_col();				//col desired move
+	if(horizontal_diff == verctical_diff ||								//check if src and dst are in diagonal line
+		-1 * (horizontal_diff) == verctical_diff);
+	{
+		(horizontal_diff > 0) ? horizontal_diff-- : horizontal_diff++;	//increases/decreases according to positivity
+		(verctical_diff > 0) ? verctical_diff-- : verctical_diff++;
+		while(horizontal_diff != 0 && verctical_diff != 0)				//goes over all the cells in the way between src and dst
+		{
+			//if there is something on the way, it is not a valid move
+			if(board_arr[dst.get_row() + horizontal_diff][dst.get_col() + verctical_diff] != nullptr)
+			{
+				return false;
+			}
+			(horizontal_diff > 0) ? horizontal_diff-- : horizontal_diff++;	//increases/decreases according to positivity
+			(verctical_diff > 0) ? verctical_diff-- : verctical_diff++;
+		}
+		return true;
+	}
+	return false;
 }
