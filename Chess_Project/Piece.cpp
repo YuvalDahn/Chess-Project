f1b97e63f08edd612a1208fd81640cc4e850856c
Yuval Dahan <yuvaldahn@gmail.com>
2018-12-07 15:18:30 +0200

    Added get_color to Side

--- Chess_Project/Piece.cpp
+++ Chess_Project/Piece.cpp
@@ -2,6 +2,11 @@
 
 Piece::~Piece() {}
 
+Side Piece::get_color()
+{
+	return this->_color;
+}
+
 bool Piece::moveStraight(Location src, Location dst)
 {
 	return src.get_col() == dst.get_col() || src.get_row() == dst.get_row();
--- Chess_Project/Piece.h
+++ Chess_Project/Piece.h
@@ -20,6 +20,7 @@ public:
 	//Piece(Side color) = 0;
 	virtual ~Piece();
 	virtual bool is_valid_move(Location src, Location dst, Piece*(*)[8]) = 0;
+	virtual Side get_color();
 	static bool moveStraight(Location src, Location dst);
 	static bool moveDiagonal(Location src, Location dst);
 };
