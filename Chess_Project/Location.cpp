37782067a9f44f85f428138bfff5568910c36cd3
Yuval Dahan <yuvaldahn@gmail.com>
2018-12-07 12:15:33 +0200

    Added getters and setters to Location

--- Chess_Project/Location.cpp
+++ Chess_Project/Location.cpp
@@ -16,3 +16,61 @@ Location::Location(char col, char row)
 Location::Location(string loc): Location(loc[0], loc[1]) {}
 
 Location::~Location() {}
+
+int Location::get_row()
+{
+	return this->_col;
+}
+
+int Location::get_col()
+{
+	return this->_row;
+}
+
+void Location::set_row(int row)
+{
+	if(row >= 0 && row <= 9)
+	{
+		this->_row = row - '0';
+	}
+	else
+	{
+		throw ILLEGAL_INDEX;
+	}
+}
+
+void Location::set_row(char row)
+{
+	if(row >= '0' && row <= '9')
+	{
+		this->_row = row - '0';
+	}
+	else
+	{
+		throw ILLEGAL_INDEX;
+	}
+}
+
+void Location::set_col(int col)
+{
+	if(col >= 0 && col <= 'z' - 'a')
+	{
+		this->_col = col - 'a';
+	}
+	else
+	{
+		throw ILLEGAL_INDEX;
+	}
+}
+
+void Location::set_col(char col)
+{
+	if(col >= 'a' && col <= 'z')
+	{
+		this->_col = col - 'a';
+	}
+	else
+	{
+		throw ILLEGAL_INDEX;
+	}
+}
--- Chess_Project/Location.h
+++ Chess_Project/Location.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include "Piece.h"
+#include "Board.h"
 
 using std::string;
 
@@ -13,4 +14,10 @@ public:
 	Location(char col, char row);
 	Location(string loc);
 	~Location();
+	int get_row();
+	int get_col();
+	void set_row(int row);
+	void set_row(char row);
+	void set_col(int col);
+	void set_col(char col);
 };
