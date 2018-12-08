cf6ec9beb11919d4f5a06899e77719ea48c72b1b
unknown <harelka2@gmail.com>
2018-12-08 02:41:55 +0200

    fixed a bug in Pawn movement where a black pawn could move up and a white pawn could move down

--- Chess_Project/Pawn.cpp
+++ Chess_Project/Pawn.cpp
@@ -9,19 +9,35 @@ Pawn::~Pawn() {}
 
 bool Pawn::is_valid_move(Location src, Location dst, Piece * board_arr[][8])
 {
-	int verctical_diff = dst.get_row() - src.get_row();
-	int horizontal_diff = dst.get_col() - src.get_col();
-	if (board_arr[dst.get_row()][dst.get_col()] == nullptr)
+	int verctical_diff = dst.get_row() - src.get_row();		 //row desired move
+	int horizontal_diff = dst.get_col() - src.get_col();	//col desired move
+	if (this->get_color())
 	{
-		if (!this->has_moved)
+		if (board_arr[dst.get_row()][dst.get_col()] == nullptr)//if you have nothing in the destination of the movement
+		{
+			if (!this->has_moved)//if the pawn moved already
+			{
+				has_moved = true;
+				return 1 <= verctical_diff && verctical_diff <= 2 && horizontal_diff == 0; //checks if it moved 1 or 2 places in the first move
+
+			}
+			return verctical_diff == 1 && horizontal_diff == 0;
+		}
+		return abs(horizontal_diff) == 1 && verctical_diff == 1;
+	}
+
+
+	if (board_arr[dst.get_row()][dst.get_col()] == nullptr)//if you have nothing in the destination of the movement
+	{
+		if (!this->has_moved)//if the pawn moved already
 		{
 			has_moved = true;
-			return (1 <= verctical_diff && verctical_diff <= 2);
+			return -1 <= verctical_diff && verctical_diff <= -2 && horizontal_diff == 0; //checks if it moved 1 or 2 places in the first move
 
 		}
-		return verctical_diff == 1;
+		return verctical_diff == -1 && horizontal_diff == 0;
 	}
-	return abs(horizontal_diff) == 1 && abs(verctical_diff) == 1;
+	return abs(horizontal_diff) == 1 && verctical_diff == -1;
 }
 	
 
