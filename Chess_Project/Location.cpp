e274f204987b251dbe1c25ba2d39452b8bf670ef
Yuval Dahan <yuvaldahn@gmail.com>
2018-12-08 02:21:36 +0200

    removed overloaded function from Location

--- Chess_Project/Location.cpp
+++ Chess_Project/Location.cpp
@@ -10,12 +10,6 @@ Location::Location(char col, char row)
 	this->set_row(row);
 }
 
-Location::Location(int col, int row)
-{
-	this->set_col(col);
-	this->set_row(row);
-}
-
 Location::~Location() {}
 
 int Location::get_row()
@@ -28,11 +22,6 @@ int Location::get_col()
 	return this->_row;
 }
 
-void Location::set_row(int row)
-{
-	this->set_row((char)row + this->MIN_ROW);
-}
-
 void Location::set_row(char row)
 {
 	if(row >= this->MIN_ROW && row <= this->MAX_ROW)
@@ -43,11 +32,6 @@ void Location::set_row(char row)
 	{
 		throw ILLEGAL_INDEX;
 	}
-}
-
-void Location::set_col(int col)
-{
-	this->set_row((char)col + this->MIN_COL);
 }
 
 void Location::set_col(char col)
--- Chess_Project/Location.h
+++ Chess_Project/Location.h
@@ -14,21 +14,18 @@ private:
 	int _col;
 	int _row;
 	
+public:
+
 	static const char MIN_ROW = '1';
 	static const char MAX_ROW = '8';
 	static const char MIN_COL = 'a';
 	static const char MAX_COL = 'h';
 
-public:
-
 	Location(char col, char row);
-	Location(int col, int row);
 	~Location();
 	int get_row();
 	int get_col();
-	void set_row(int row);
 	void set_row(char row);
-	void set_col(int col);
 	void set_col(char col);
 
 };
