e0e09021f1e62fc03975976c645f3e65cc685d3e
Yuval Dahan <yuvaldahn@gmail.com>
2018-12-12 16:49:43 +0200

    Fixed spacing in Pawn

--- Chess_Project/Pawn.cpp
+++ Chess_Project/Pawn.cpp
@@ -38,12 +38,12 @@ bool Pawn::is_valid_move(Location src, Location dst, Piece * board_arr[][8])
 	}
 
 
-	if (board_arr[dst.get_row()][dst.get_col()] == nullptr)//if you have nothing in the destination of the movement
+	if (board_arr[dst.get_row()][dst.get_col()] == nullptr)	//if you have nothing in the destination of the movement
 	{
-		if (!this->has_moved)//if the pawn moved already
+		if (!this->has_moved)	//if the pawn didn't moved
 		{
-			has_moved = true;
-			return -1 <= verctical_diff && verctical_diff <= -2 && horizontal_diff == 0; //checks if it moved 1 or 2 places in the first move
+			this->has_moved = true;
+			return -1 >= verctical_diff && verctical_diff >= -2 && horizontal_diff == 0; //checks if it moved 1 or 2 places in the first move
 
 		}
 		return verctical_diff == -1 && horizontal_diff == 0;
